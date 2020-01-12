3db028a6ba57bb0343c3f3e02ab6750e2138b278
inaut6ll <milanzq309@gmail.com>
2020-01-12 05:16:58 -0800

    changed format of some stuff
    to see whats wrong with printmaze

--- Driver.java
+++ Driver.java
@@ -3,17 +3,20 @@ public class Driver
     public static void main() 
     {
         Maze maze1 = new Maze("maze1.txt");
-        Maze maze2 = new Maze("maze2.txt");
-        Maze maze3 = new Maze("maze3.txt");
-        
         maze1.read();
-        
-        //maze1.printMaze();
-        //maze2.printMaze();
-        //maze3.printMaze();
-        
+        maze1.printMaze();
         maze1.isSolvable();
+        System.out.println();
+        
+        Maze maze2 = new Maze("maze2.txt");
+        maze2.read();
+        maze2.printMaze();
         maze2.isSolvable();
+        System.out.println();
+        
+        Maze maze3 = new Maze("maze3.txt");
+        maze3.read();
+        //maze3.printMaze();
         maze3.isSolvable();
     }
 }
